c5ca6e64e209ed23e160e88aae8c62588e85a907
Frozik6k <Frozik6k@yandex.ru>
2025-05-22 13:09:24 +0500

    Миграции и индексы

---
+++ src/main/resources/liquibase/scripts/v1__student_name_index.sql
@@ -0,0 +1,4 @@
+-- liguibase formatted sql
+
+-- changeset frozik6k:1
+CREATE INDEX IF NOT EXISTS student_name_index ON student (name)
---
+++ src/main/resources/liquibase/scripts/v2__faculty_name_color_index.sql
@@ -0,0 +1,4 @@
+-- liguibase formatted sql
+
+-- changeset frozik6k:2
+CREATE INDEX faculty_name_color_index ON faculty (name, color);
